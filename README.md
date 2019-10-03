98cf3a0ca64e9c5a0415d744a04a6897ee726d63
Souls362 <sqiworking@gmail.com>
2019-10-03 21:08:38 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# MyResume
-My Resume
+# 动态简历(animate_resume)
+
+## 在线观看 view online
+
+https://qishaoxuan.github.io/animate_resume_ts/
+
+## 关于 features
+
+- typescript
+- 字体样式参考 prismjs
+- prismjs + marked 处理代码转换
+- 移动端使用了 better-scroll 优化体验
+
+## 讲解
+
+[又双叒叕是一个动态简历](https://juejin.im/post/5c1fd3f1f265da61171cb8bf)
+
+## 本地查看
+
+```bash
+  git clone git@github.com:QiShaoXuan/animate_resume_ts.git
+  cd animate_resume_ts
+  npm install
+  gulp
+```
+
+## 修改
+
+style 和 resume 在 `src/load/` 下的对应js中
+
+也可以直接引用 `dist` 下的文件，使用参考 `src/scripts/index.ts`
+
+## 打包
+
+```bash
+  gulp build
+```
+
+参考：
+
+> http://strml.net/
---
+++ src/load/resume.ts
@@ -0,0 +1,33 @@
+export const resume:string = `# SIHUA QI (Frank)
+
+# Contact
+sqiclemson@gmail.com
+
+# Bio
+* I am currently working at Ping An Insurance Group as an algorithm engineer. 
+My working area includes Knowledge Graph-based Dialogue System (Medical QA System), Natural Language to SQL, Information Extraction. I am particularly interested in applying machine learning and natural language processing techniques in healthcare and finance. 
+
+# Career
+* Ping An Insurance (Group) Company of China, Ltd.
+* Baidu, Inc
+* AnyPromo, Inc
+
+# Education
+* The George Washington University Washington, DC
+* Clemson University Clemson, SC
+* East China Normal University Shanghai, China
+
+# Research Area
+* Knowledge Graph-based Dialogue System
+* Information Extraction/ Named Entity Recognition
+* Natural Language to SQL
+* NLP Applications in Healthcare/Finance
+
+# Conference Papers
+* SiHua Qi, Lulwah AlKulaib and David A Broniatowski. Detecting and Characterizing Bot-Like Behavior on Twitter. In Proceedings of the Conference on SBP-BRiMS, 2018 
+* David A Broniatowski, Amelia M Jamison, SiHua Qi, Lulwah AlKulaib, Tao Chen, Adrian Benton, Sandra C Quinn, Mark Dredze. Weaponized health communication: Twitter bots and Russian trolls amplify the vaccine debate. In American Journal of Public Health, 2018
+
+
+# Link:
+https://github.com/Souls362
+`
